f3ac4769a1a92209d746635e40982321865e6ef8
Dennis Brink <dennis.brink@villaforyou.com>
2024-03-17 19:57:50 +0100

    ep-4 ep-5

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="app">
-    <div>
-      {{ jobs }}
-    </div>
+    <header>
+      <div class="order">
+        <button @click="handleClick(`title`)">Order by title</button>
+        <button @click="handleClick(`salary`)">Order by salary</button>
+        <button @click="handleClick(`location`)">Order by location</button>
+      </div>
+    </header>
+    <JobsList :jobs="jobs" :order="order"/>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
-import { Job } from './types/types'
+import JobsList from './components/JobsList.vue'
+import { Job, OrderTerm } from './types/types'
 
 export default defineComponent({
   name: 'App',
-  components: {},
+  components: {JobsList},
   setup() {
     const jobs = ref<Job[]>([
       {title: 'Test Engineer', location: 'Bangalore, India', salary: 1000, id: '1'},
@@ -21,11 +27,32 @@ export default defineComponent({
       {title: 'JS/ABL developer', location: 'Helmond, Netherlands', salary: 19000, id: '4'},
       {title: 'Test Engineer (Junior)', location: 'Bangalore, India', salary: 750, id: '5'},
     ])
-    return { jobs }
+
+    const order = ref<OrderTerm>('title')
+    const handleClick = (term: OrderTerm) => {
+      order.value = term
+    }
+
+    return { jobs, handleClick, order }
   }
 })
 </script>
 
-<style scoped>
-
+<style>
+  header {
+    text-align: center;
+  }
+  header .order {
+    margin-top: 20px;
+  }
+  button {
+    margin: 0 10px;
+    color: #1195c9;
+    border: 3px solid #1195c9;
+    background: #d5f0ff; 
+    padding: 8px 16px; 
+    border-radius: 4px; 
+    cursor: pointer; 
+    font-weight: bold;
+  }
 </style>
